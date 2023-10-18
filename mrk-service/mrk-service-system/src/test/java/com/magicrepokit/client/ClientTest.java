6e25f04ffac5eecfb1564d59571aff04ec9da1e6
AuroraPixel <wangailiziwen@gmail.com>
2023-10-18 15:14:16 +0800

    oauth增强

--- mrk-service/mrk-service-system/src/test/java/com/magicrepokit/client/ClientTest.java
+++ mrk-service/mrk-service-system/src/test/java/com/magicrepokit/client/ClientTest.java
@@ -16,25 +16,5 @@ import java.util.Collections;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ComponentScan("com.magicrepokit")
 public class ClientTest {
-    @Autowired
-    private IOAuth2ClientService ioAuth2ClientService;
 
-    @Test
-    public void setClient(){
-        AuthClient client = new AuthClient();
-        client.setClientId("test")
-                .setSecret(BCrypt.hashpw("test"))
-                .setName("测试")
-                .setLogo("")
-                .setDescription("测试应用")
-                .setStatus(0)
-                .setAccessTokenValiditySeconds(30*60)
-                .setRefreshTokenValiditySeconds(3600*24*7)
-                .setRedirectUris(Collections.singletonList("http://localhost:8080/test"))
-                .setAuthorizedGrantTypes(Arrays.asList("password","code","refreshToken"))
-                .setScopes(Arrays.asList("user:read","user:write"))
-                .setAuthorities(Arrays.asList("user:read","user:write"))
-                .setResourceIds(Arrays.asList("1","2"));
-        ioAuth2ClientService.save(client);
-    }
 }
