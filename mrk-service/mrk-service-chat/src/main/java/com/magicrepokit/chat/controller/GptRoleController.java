7f2b0e3a7492028e90f3a1f66786365ca08d6a1b
AuroraPixel <wangailiziwen@gmail.com>
2024-01-11 08:41:14 +0800

    开始GPT角色建立

---
+++ mrk-service/mrk-service-chat/src/main/java/com/magicrepokit/chat/controller/GptRoleController.java
@@ -0,0 +1,19 @@
+package com.magicrepokit.chat.controller;
+
+import io.swagger.annotations.Api;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * GPT角色管理接口
+ * @author AuroraPixel
+ * @github https://github.com/AuroraPixel
+ */
+@RestController
+@RequestMapping("role")
+@AllArgsConstructor
+@Api(value = "GPT角色管理接口", tags = "GPT角色管理接口")
+public class GptRoleController {
+
+}
